a1a8109c88e2b070b5db6919fa37c11c39f4a7ec
Manfred Ramos Macías <45468371+manfredramos97@users.noreply.github.com>
2025-04-12 10:40:46 -0600

    prueba cognito Update config.js
    prueba

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_rtVL3cMR7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6l6q4vaja3ftvlopgget6mn6p3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
